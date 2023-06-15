76b1b8351b6643c6e2a58a276c1c779cd3fbf804
Lukhas <lbonnet@bearstech.com>
2023-06-15 21:27:58 +0100

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+use std::io;
+use tokio_i3ipc::{
+    event::{Event, Subscribe},
+    I3,
+};
+use tokio_stream::StreamExt;
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> io::Result<()> {
+    let mut i3 = I3::connect().await?;
+    let resp = i3.subscribe([Subscribe::Window]).await?;
+
+    println!("{:#?}", resp);
+    let mut listener = i3.listen();
+    while let Some(event) = listener.next().await {
+        match event? {
+            Event::Workspace(ev) => println!("workspace change event {:?}", ev),
+            Event::Window(ev) => println!("window event {:?}", ev),
+            Event::Output(ev) => println!("output event {:?}", ev),
+            Event::Mode(ev) => println!("mode event {:?}", ev),
+            Event::BarConfig(ev) => println!("bar config update {:?}", ev),
+            Event::Binding(ev) => println!("binding event {:?}", ev),
+            Event::Shutdown(ev) => println!("shutdown event {:?}", ev),
+            Event::Tick(ev) => println!("tick event {:?}", ev),
+        }
+    }
+    Ok(())
+}
